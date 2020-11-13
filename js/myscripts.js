d55ddafd0e238da5c4910a70d5287cac9521da1b
Marine Auribault <marineauribault@MacBook-Pro-de-Marine-2.local>
2020-11-13 17:50:01 +0100

    Finished

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,6 +1,7 @@
-document.addEventListener('DOMCContentLoaded', function()) {
+$( document ).ready(function() {
 
-    $('form').submit(function(e){
+
+    $('form').on("submit",function(e){
 
             $('#errorFirstName').attr('hidden', true)
             $('#errorLastName').attr('hidden', true)
@@ -12,49 +13,85 @@ document.addEventListener('DOMCContentLoaded', function()) {
 
             let valid = true;
             
-            let firstname = $ ('#firstName').val();
+            let firstname = $('#firstName').val();
             
-            if(firstName === '' || firstname === 'undefined'){
+            if(firstname === '' || firstname === 'undefined'){
                 $('#errorFirstName').removeAttr('hidden');
                 valid = false;
             }
+            else{
+                $('#errorFirstName').attr('hidden');
+                valid = true;
+            }
 
-            let lastName = $ ('#lastName').val();
-            if(lastName === '' || lirstname === 'undefined'){
+            let lastName = $('#lastName').val();
+            if(lastName === '' || lastName === 'undefined'){
                 $('#errorLastName').removeAttr('hidden');
                 valid = false;
             }
+            else{
+                $('#errorLirstName').attr('hidden');
+                valid = true;
+            }
 
-            let email = $ ('#email').val();
+            let email = $('#email').val();
             if(email === '' || email === 'undefined'){
                 $('#errorEmail').removeAttr('hidden');
                 valid = false;
             }
+            else{
+                $('#errorEmail').attr('hidden');
+                valid = true;
+            }
 
-            let password = $ ('#password').val();
+            let password = $('#password').val();
             if(password === '' || password === 'undefined'){
                 $('#errorPassword').removeAttr('hidden');
                 valid = false;
             }
+            else{
+                $('#errorPassword').attr('hidden');
+                valid = true;
+            }
 
-            let checkboxMale = $ ('#male').is (':checked')
-            let checkboxFemale = $ ('#female').is (':checked')
+            let checkboxMale = $('#male').is(':checked')
+            let checkboxFemale = $('#female').is(':checked')
 
             if(checkboxMale === false && checkboxFemale === false){
-                $('errorCheckbox').removeAttr('hidden')
+                $('#errorCheckbox').removeAttr('hidden')
                 valid = false
             }
+            else{
+                $('#errorCheckbox').attr('hidden');
+                valid = true;
+            }
 
-            let checkConditions = $('#tos').is (':checked')
+            let checkConditions = $('#tos').is(':checked')
             if(checkConditions === false ){
-                $('errorTos').removeAttr('hidden')
+                $('#errorTos').removeAttr('hidden')
                 valid = false
+            }
+            else{
+                $('#errorTos').attr('hidden');
+                valid = true;
+            }
+
+            let skills = $('#skills').val()
+            if(skills == 'Choose...'){
+                $('#errorSkills').removeAttr('hidden')
+                valid = false;
+            }
+            else{
+                $('#errorSkills').attr('hidden');
+                valid = true;
             }
 
 
 
 
-            e.preventDefault();
+            if(!valid){
+                e.preventDefault();
+            }
 
     })
 
